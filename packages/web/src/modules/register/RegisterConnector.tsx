fad571db8e95fb9705f75193fc460c5d2abe8be5
kedmenecr <kedmenec.roberto@gmail.com>
2018-07-06 11:30:36 +0200

    Add validation on login.

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,4 +1,15 @@
 import * as React from 'react'
-import RegisterView from './ui/RegisterView';
+import { RegisterView } from './ui/RegisterView';
 
-export const RegisterConnector = () => <RegisterView />
+export class RegisterConnector extends React.PureComponent {
+
+  dummySubmit = async (values: any) => {
+    console.log('values :', values);
+    return null;
+  }
+
+  render() {
+    return <RegisterView submit={this.dummySubmit} />;
+
+  }
+}
--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,35 +1,100 @@
-import * as React from 'react';
-import { Form, Icon, Input, Button } from 'antd';
+import * as React from "react";
+import { Form, Icon, Input, Button } from "antd";
+import { withFormik, FormikErrors, FormikProps } from "formik";
+import * as yup from 'yup';
 
 const FormItem = Form.Item;
 
-export default class RegisterView extends React.PureComponent {
-  render() {
+interface FormValues {
+  email: string;
+  password: string;
+}
 
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+class C extends React.PureComponent<FormikProps<FormValues> & Props> {
+  render() {
+    const { values, handleChange, handleBlur, handleSubmit, touched, errors } = this.props;
+    console.log(errors);
     return (
-      <div style={{ display: 'flex' }}>
-        <div style={{ width: 400, margin: 'auto' }}>
-          <FormItem>
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username" />
+      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+        <div style={{ width: 400, margin: "auto" }}>
+          <FormItem
+            help={touched.email && errors.email ? errors.email : ''}
+            validateStatus={touched.email && errors.email ? "error" : undefined}
+          >
+            <Input
+              name="email"
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </FormItem>
+          <FormItem
+            help={touched.password && errors.password ? errors.password : ''}
+            validateStatus={touched.password && errors.password ? "error" : undefined}>
+            <Input
+              name="password"
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
           </FormItem>
           <FormItem>
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password"
-              placeholder="Password" />
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
           </FormItem>
           <FormItem>
-            <a className="login-form-forgot" href="">Forgot password</a>
-          </FormItem>
-          <FormItem>
-            <Button type="primary" htmlType="submit" className="login-form-button">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
               Register
-          </Button>
+            </Button>
           </FormItem>
           <FormItem>
-            Or <a href="">Login now!</a>
+            Or <a href="">login now!</a>
           </FormItem>
-        </div >
-      </div>
+        </div>
+      </form>
     );
   }
 }
+
+const emailNotLongEnough = "email must be at least 3 characters";
+const passwordNotLongEnough = "password must be at least 3 characters";
+const invalidEmail = "email must be a valid email";
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail)
+    .required(),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .required()
+    .max(255)
+});
+
+export const RegisterView = withFormik<Props, FormValues>({
+  validationSchema,
+  mapPropsToValues: () => ({ email: "", password: "" }),
+  handleSubmit: async (values, { props, setErrors }) => {
+    const errors = await props.submit(values);
+    if (errors) {
+      setErrors(errors);
+    }
+  }
+})(C);
