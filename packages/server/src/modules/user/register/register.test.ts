e14bd71cf236438d587683e8fb0cb3985b097227
kedmenecr <kedmenec.roberto@gmail.com>
2018-07-12 12:49:42 +0200

    Add code sharing with the common folder.

--- packages/server/src/modules/user/register/register.test.ts
+++ packages/server/src/modules/user/register/register.test.ts
@@ -1,12 +1,14 @@
 import { Connection } from "typeorm";
 import * as faker from "faker";
-
-import { User } from "../../../entity/User";
 import {
-  duplicateEmail,
   emailNotLongEnough,
   invalidEmail,
   passwordNotLongEnough
+} from "@car-share/common";
+import { User } from "../../../entity/User";
+import {
+  duplicateEmail,
+
 } from "./errorMessages";
 import { TestClient } from "../../../utils/TestClient";
 import { createTestConn } from "../../../testUtils/createTestConn";
